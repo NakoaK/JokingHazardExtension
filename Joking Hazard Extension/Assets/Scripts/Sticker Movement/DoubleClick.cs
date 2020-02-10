f715e0e035ec7f54960703acc43c8b306499b731
S4C-Olympus <54639306+S4C-Olympus@users.noreply.github.com>
2020-02-09 23:59:59 -0700

    Sticker Prefabs

---
+++ Joking Hazard Extension/Assets/Scripts/Sticker Movement/DoubleClick.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleClick : MonoBehaviour
+{
+    public float firstClickTime, timeBetweenClicks;
+    private bool coroutineAllowed;
+    private int clickCounter;
+    private int flip = 180;
+    void Start()
+    {
+        firstClickTime = 0f;
+        timeBetweenClicks = 0.2f;
+        clickCounter = 0;
+        coroutineAllowed = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            clickCounter += 1;
+        }
+
+        if (clickCounter == 1 && coroutineAllowed)
+        {
+            firstClickTime = Time.time;
+            StartCoroutine(DoubleClickDetection());
+        }
+    }
+
+    private IEnumerator DoubleClickDetection()
+    {
+        coroutineAllowed = false;
+        while (Time.time < firstClickTime + timeBetweenClicks)
+        {
+            if (clickCounter == 2)
+            {
+                Debug.Log("Double Click");
+                transform.rotation = Quaternion.Euler(0, flip, 0);
+                flip += 180;
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        clickCounter = 0;
+        firstClickTime = 0f;
+        coroutineAllowed = true;
+
+    }
+}
