e16872bc255aa05dd45b1982ae2f72f938692930
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2020-02-18 19:59:57 -0700

    Text Spawning works

---
+++ Joking Hazard Extension/Assets/Scripts/Text/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string input;
+    public GameObject playerText;
+    public GameObject display;
+
+    public void StoreInput()
+    {
+        input = playerText.GetComponent<TextMeshProUGUI>().text;
+        print(input);
+        display.GetComponent<TextMeshProUGUI>().text = input;
+    }
+
+}
