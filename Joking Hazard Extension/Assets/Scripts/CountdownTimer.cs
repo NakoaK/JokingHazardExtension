b49f37ab1ffd0bffbf677e3ec6f2bc8e2f04f208
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2020-02-22 12:49:47 -0700

    timer and start pause stop buttons choose randopm song every time

---
+++ Joking Hazard Extension/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string tempTime;
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool doOnce = false;
+    private bool startedCount = false;
+    private bool paused = false;
+    public TextMeshProUGUI showTime;
+
+    private void Start()
+    {
+        timer = mainTimer;
+    }
+
+    private void Update()
+    {
+        if (timer >= 0f && startedCount)
+        {
+            timer -= Time.deltaTime;
+            tempTime = timer.ToString("F");
+            showTime.text = tempTime;
+        }
+        else if (timer <= 0f && !doOnce)
+        {
+            doOnce = true;
+            startedCount = false;
+            showTime.text = "0.00";
+            timer = 0.0f;
+        }
+    }
+
+    public void resetTimer()
+    {
+        timer = mainTimer;
+        startedCount = true;
+        doOnce = false;
+    }
+
+    public void StartCount()
+    {
+        if(!startedCount)
+        {
+            startedCount = true;
+            timer = mainTimer;
+            Time.timeScale = 1;
+        }
+       
+    }
+
+    public void stopCount()
+    {
+        startedCount = false;
+        showTime.text = "60";
+        paused = false;
+
+        doOnce = false;
+    }
+
+    public void pauseCount()
+    {
+        if(startedCount)
+        {
+            if (!paused)
+        {
+            Time.timeScale = 0;
+            paused = true;
+        }
+        else if (paused)
+        {
+            Time.timeScale = 1;
+            paused = false;
+        }
+        }
+        
+    }
+}
--- Joking Hazard Extension/Assets/Scripts/MusicTimer.cs
+++ Joking Hazard Extension/Assets/Scripts/MusicTimer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicTimer : MonoBehaviour
 {
@@ -11,14 +12,30 @@ public class MusicTimer : MonoBehaviour
     public bool pauseOn;
     public bool started;
 
+    public Sprite startOFF;
+    public Sprite startON;
+    public Sprite pauseOFF;
+    public Sprite pauseON;
+
+    public GameObject startButton;
+    public GameObject pauseButton;
+
+    public AudioSource BG;
+    public AudioClip theme;
+    public AudioClip lab;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        timerCount = 5f;
+
+        timerCount = 60f;
         started = false;
         timerOn = false;
         pauseOn = false;
         timeLeft = timerCount;
+
+        startButton.GetComponent<Image>().sprite = startON;
     }
 
     // Update is called once per frame
@@ -39,30 +56,52 @@ public class MusicTimer : MonoBehaviour
     {
         if(!started)
         {
+            int song = Random.Range(0, 4);
+            if (song == 0 || song == 1 || song == 2)
+            {
+                this.GetComponent<AudioSource>().clip = theme;
+            }
+            else
+            {
+                this.GetComponent<AudioSource>().clip = lab;
+
+            }
+
             timeLeft = timerCount;
             timerOn = true;
             musicSource.Play();
             started = true;
+
+            startButton.GetComponent<Image>().sprite = startOFF;
+
         }
     }
 
     public void pausePressed()
     {
-        if(!pauseOn)
+        if(started)
         {
-            pauseOn = true;
-            Time.timeScale = 0;
-            musicSource.Pause();
-            print("pressed pause");
+            if (!pauseOn)
+            {
+                pauseOn = true;
+                Time.timeScale = 0;
+                musicSource.Pause();
+                pauseButton.GetComponent<Image>().sprite = pauseON;
+
+                print("pressed pause");
+            }
+            else if (pauseOn)
+            {
+                pauseOn = false;
+                Time.timeScale = 1;
+                musicSource.UnPause();
+                pauseButton.GetComponent<Image>().sprite = pauseOFF;
+
+                print("Unpaused");
+
+            }
         }
-        else if(pauseOn)
-        {
-            pauseOn = false;
-            Time.timeScale = 1;
-            musicSource.UnPause();
-            print("Unpaused");
-            
-        }
+        
 
     }
 
@@ -73,5 +112,8 @@ public class MusicTimer : MonoBehaviour
         timerOn = false;
         pauseOn = false;
         Time.timeScale = 1;
+        startButton.GetComponent<Image>().sprite = startON;
+        pauseButton.GetComponent<Image>().sprite = pauseOFF;
+
     }
 }
